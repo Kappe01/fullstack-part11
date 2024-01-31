7c111b5eb62a8182c769431d7e20b283fb8c4c33
Kasper Envalds <kasper.envalds@gmail.com>
2024-01-31 22:19:09 +0200

    .

--- backend/tests/blog_api.test.js
+++ backend/tests/blog_api.test.js
@@ -1,69 +1,65 @@
-const mongoose = require('mongoose')
-const supertest = require('supertest')
-const app = require('../app')
+const mongoose = require("mongoose");
+const supertest = require("supertest");
+const app = require("../app");
 
-const api = supertest(app)
+const api = supertest(app);
 
-const Blog = require('../models/blog')
-const helper = require('./test_helper')
+const Blog = require("../models/blog");
+const helper = require("./test_helper");
 
 beforeEach(async () => {
-  await Blog.deleteMany({})
-  await Blog.insertMany(helper.initialBlogs)
-})
+  await Blog.deleteMany({});
+  await Blog.insertMany(helper.initialBlogs);
+});
 
-test('blogs are returned as json', async () => {
+test("blogs are returned as json", async () => {
   await api
-    .get('/api/blogs')
+    .get("/api/blogs")
     .expect(200)
-    .expect('Content-type', /application\/json/)
-})
+    .expect("Content-type", /application\/json/);
+});
 
-test('blogs have identifier named id', async () => {
-  const response = await api.get('/api/blogs')
+test("blogs have identifier named id", async () => {
+  const response = await api.get("/api/blogs");
 
-  const blogs = response.body
+  const blogs = response.body;
 
-  blogs.forEach(blog => {
-    expect(blog.id).toBeDefined()
-  })
-})
+  blogs.forEach((blog) => {
+    expect(blog.id).toBeDefined();
+  });
+});
 
-test('a valid blog can be added', async () => {
+test("a valid blog can be added", async () => {
   const newBlog = {
-    _id: '5a422aa71b54a676234d17f9',
-    title: 'test',
-    author: 'Tester',
-    url: 'http://www.test.com',
+    _id: "5a422aa71b54a676234d17f9",
+    title: "test",
+    author: "Tester",
+    url: "http://www.test.com",
     likes: 10,
-    __v: 0
-  }
+    __v: 0,
+  };
 
   await api
-    .post('/api/blogs')
+    .post("/api/blogs")
     .send(newBlog)
     .expect(201)
-    .expect('Content-Type', /application\/json/)
+    .expect("Content-Type", /application\/json/);
 
-  const blogsAtEnd = await helper.blogsInDb()
-  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
-})
+  const blogsAtEnd = await helper.blogsInDb();
+  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1);
+});
 
-test('a blog can be deleted', async () => {
-  const blogsAtStart = await helper.blogsInDb()
-  const blogToDelete = blogsAtStart[0]
+test("a blog can be deleted", async () => {
+  const blogsAtStart = await helper.blogsInDb();
+  const blogToDelete = blogsAtStart[0];
 
-  await api
-    .delete(`/api/blogs/${blogToDelete.id}`)
-    .expect(204)
+  await api.delete(`/api/blogs/${blogToDelete.id}`).expect(204);
 
-  const blogsAtEnd = await helper.blogsInDb()
+  const blogsAtEnd = await helper.blogsInDb();
 
-  expect(blogsAtEnd).toHaveLength(
-    helper.initialBlogs.length - 1
-  )
-})
+  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length - 1);
+});
 
 afterAll(async () => {
-  await mongoose.connection.close()
-})
+  await mongoose.connection.close();
+});
--- bloglist-frontend/cypress/e2e/blog_app.cy.js
+++ bloglist-frontend/cypress/e2e/blog_app.cy.js
@@ -1,113 +1,112 @@
-describe('Blog ', function() {
-beforeEach(function() {
-  cy.request('POST', 'http://localhost:3003/api/testing/reset')
+describe("Blog ", function () {
+  beforeEach(function () {
+    cy.request("POST", "http://localhost:3003/api/testing/reset");
     const user = {
-      name: 'Kasper Envalds',
-      username: 'kappe',
-      password: 'kappe1234'
-    }
-    cy.request('POST', 'http://localhost:3003/api/users/', user) 
-    cy.visit('http://localhost:5173')
-})
+      name: "Kasper Envalds",
+      username: "kappe",
+      password: "kappe1234",
+    };
+    cy.request("POST", "http://localhost:3003/api/users/", user);
+    cy.visit("http://localhost:5173");
+  });
 
-  it('front page can be opened', function() {
-    cy.contains('login')
-  })
+  it("front page can be opened", function () {
+    cy.contains("login");
+  });
 
-  it('Login form is shown', function() {
-    cy.contains('login').click()
-  })
+  it("Login form is shown", function () {
+    cy.contains("login").click();
+  });
 
-  describe('Login', function() {
-    it('user can login', function() {
-      cy.contains('login').click()
-      cy.get('#username').type('kappe')
-      cy.get('#password').type('kappe1234')
-      cy.get('#login-button').click()
-  
-      cy.contains('new blog')
-    })
+  describe("Login", function () {
+    it("user can login", function () {
+      cy.contains("login").click();
+      cy.get("#username").type("kappe");
+      cy.get("#password").type("kappe1234");
+      cy.get("#login-button").click();
 
-    it('user cannot login with incorrect credentials', function() {
-      cy.contains('login').click()
-      cy.get('#username').type('kappe')
-      cy.get('#password').type('wrong')
-      cy.get('#login-button').click()
-  
-      cy.contains('wrong credentials')
-    })
-  })
+      cy.contains("new blog");
+    });
 
-  describe('when logged in', function() {
-    beforeEach(function() {
-      cy.login({ username: 'kappe', password: 'kappe1234' })
-    })
+    it("user cannot login with incorrect credentials", function () {
+      cy.contains("login").click();
+      cy.get("#username").type("kappe");
+      cy.get("#password").type("wrong");
+      cy.get("#login-button").click();
 
-    it('a new blog can be created', function() {
-      cy.contains('new blog').click()
-      cy.get('#title').type('test_title')
-      cy.get('#author').type('test_author')
-      cy.get('#url').type('http://testurl.com')
-      cy.contains('create').click()
-      cy.contains('test_title')
-    })
+      cy.contains("wrong credentials");
+    });
+  });
 
-    describe('when blog created', function() {
-      beforeEach(function() {
+  describe("when logged in", function () {
+    beforeEach(function () {
+      cy.login({ username: "kappe", password: "kappe1234" });
+    });
+
+    it("a new blog can be created", function () {
+      cy.contains("new blog").click();
+      cy.get("#title").type("test_title");
+      cy.get("#author").type("test_author");
+      cy.get("#url").type("http://testurl.com");
+      cy.contains("create").click();
+      cy.contains("test_title");
+    });
+
+    describe("when blog created", function () {
+      beforeEach(function () {
         cy.createBlog({
-          title: 'test_title',
-          author: 'test_author',
+          title: "test_title",
+          author: "test_author",
           likes: 0,
-          url: 'http://test_url.com'
-        })
-      })
+          url: "http://test_url.com",
+        });
+      });
 
-      it('a blog can be liked', function() {
-        cy.contains('view').click()
-        cy.contains('like').click()
-        cy.contains('1')
-      })
+      it("a blog can be liked", function () {
+        cy.contains("view").click();
+        cy.contains("like").click();
+        cy.contains("1");
+      });
 
-      it('a blog can be removed', function() {
-        cy.contains('view').click()
-        cy.contains('remove').click()
-        cy.get('html').should('not.contain', 'test_title')
-      })
+      it("a blog can be removed", function () {
+        cy.contains("view").click();
+        cy.contains("remove").click();
+        cy.get("html").should("not.contain", "test_title");
+      });
 
-      it('only blog creator sees remove button', function() {
+      it("only blog creator sees remove button", function () {
         const user = {
-          name: 'Kasper',
-          username: 'kappe123',
-          password: 'kappe'
-        }
-        cy.request('POST', 'http://localhost:3003/api/users/', user) 
-        cy.visit('http://localhost:5173')
+          name: "Kasper",
+          username: "kappe123",
+          password: "kappe",
+        };
+        cy.request("POST", "http://localhost:3003/api/users/", user);
+        cy.visit("http://localhost:5173");
 
-        cy.login({ username: 'kappe123', password: 'kappe'})
+        cy.login({ username: "kappe123", password: "kappe" });
 
-        cy.contains('view').click()
+        cy.contains("view").click();
 
-        cy.get('button').should('not.contain', 'remove')
-      })
+        cy.get("button").should("not.contain", "remove");
+      });
 
-      it('blogs ordered by most likes', function() {
+      it("blogs ordered by most likes", function () {
         cy.createBlog({
-          title: 'test_title1',
-          author: 'test_author',
+          title: "test_title1",
+          author: "test_author",
           likes: 10,
-          url: 'http://test_url.com'
-        })
+          url: "http://test_url.com",
+        });
         cy.createBlog({
-          title: 'test_title_most',
-          author: 'test_author',
+          title: "test_title_most",
+          author: "test_author",
           likes: 20,
-          url: 'http://test_url.com'
-        })
-        
-        cy.get('.blog').eq(0).should('contain', 'test_title_most')
-        cy.get('.blog').eq(1).should('contain', 'test_title1')
-      })
-    })  
+          url: "http://test_url.com",
+        });
 
-  })
-})
+        cy.get(".blog").eq(0).should("contain", "test_title_most");
+        cy.get(".blog").eq(1).should("contain", "test_title1");
+      });
+    });
+  });
+});
